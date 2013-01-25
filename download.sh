54865691558917a9c9723d54955e810d9d7279cd
Alexander <alxndr+github@gmail.com>
2013-01-25 10:18:38 -0800

    download.sh: adding python, ruby, perl Fixes #1

--- download.sh
+++ download.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# todo add ruby, python, perl
-
 if [[ $# -ne 1 ]]; then
   echo "Usage: $(basename $0) url"
   echo -e "\tDownloads url and spits it out on STDOUT."
-  echo -e "\tUses wget, lynx, php, (e?)links, or lwp-download, whichever is available"
+  echo -e "\tUses wget, lynx, python, ruby, perl, php, (e?)links, or lwp-download, whichever is available"
   echo -e "\tUser agent defaults to Firefox 3.6; override by setting \$USERAGENT"
   echo
   exit 1
@@ -27,9 +25,27 @@ if [[ "$(is_available wget)" ]]; then
 elif [[ "$(is_available lynx)" ]]; then
   echo "using lynx" 1>&2
   lynx -source "$1" -useragent="$USERAGENT"
-elif [[ "$(is_available php)" && -z "$(php -r 'file_get_contents(\'http://www.google.com/\');' | grep 'failed to open stream')" ]]; then # test URL here needs to be quoted with \'... \" doesn't work
+elif [[ "$(is_available python)" && -n "$(python -c 'import urllib2; print urllib2' | grep from)" ]]; then
+  echo "using python" 1>&2
+  python -c "
+    import urllib2
+    print urllib2.urlopen(urllib2.Request('$1', None, {'User-Agent':'$USERAGENT'})).read()
+  "
+elif [[ "$(is_available ruby)" ]]; then
+  echo "using ruby" 1>&2
+  ruby -r open-uri -e '
+    print open("$1", "User-Agent" => "$USERAGENT").read
+  '
+elif [[ "$(is_available perl)" ]]; then
+  echo "using perl" 1>&2
+  perl -MLWP::UserAgent -e '
+    print LWP::UserAgent->new("agent"=>"$USERAGENT")->get("$1")->as_string
+  '
+elif [[ "$(is_available php)" && -n "$(php -r 'print file_get_contents(\"http://www.google.com/\"');' | grep -i doctype" ]]; then
   echo "using php" 1>&2
-  php -r 'print file_get_contents("$1",FILE_TEXT,stream_context_create(array("user_agent"=>"$USERAGENT")));'
+  php -r '
+    print file_get_contents("$1",FILE_TEXT,stream_context_create(array("user_agent"=>"$USERAGENT")));
+  '
 elif [[ "$(is_available elinks)" ]]; then
   echo "using elinks" 1>&2
   elinks -source "$1"
@@ -40,7 +56,7 @@ elif [[ "$(is_available lwp-download)" ]]; then
   echo "using lwp-download" 1>&2
   lwp-download "$1" -
 else
-  echo "ERROR -- couldn't find wget, lynx, php, elinks, links, or lwp-download..." 1>&2
+  echo "ERROR -- couldn't find wget, lynx, python, ruby, perl, php, elinks, links, or lwp-download..." 1>&2
   echo "(make sure one is executable and discoverable by which)" 1>&2
   echo "...dying" 1>&2
   echo 1>&2
