e5e43d025f74c02b7ef2bf8ec3435650f3fb6fd6
Alexander <alxndr+github@gmail.com>
2014-02-14 22:25:31 -0800

    elixir: etude 3-1

--- etudes-for-elixir/geom.exs
+++ etudes-for-elixir/geom.exs
@@ -1,7 +1,12 @@
 defmodule Geom do
 
-  def area(w // 1, l // 1) do
-    w * l
+  def area(shape, a, b//1) do
+    case shape do
+      :triangle -> a * b / 2
+      :rectangle -> a * b
+      :ellipse -> :math.pi() * a * b
+      _ -> 'you done fucked up'
+    end
   end
 
 end
