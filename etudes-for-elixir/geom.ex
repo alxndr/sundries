fc23f53365773064a531502731f53af90b8400ab
Alexander <alxndr+github@gmail.com>
2014-02-17 17:36:15 -0800

    elixir: etudes 3-2: split apart Geom.area/2

--- etudes-for-elixir/geom.ex
+++ etudes-for-elixir/geom.ex
@@ -2,13 +2,16 @@ defmodule Geom do
 
   @spec area(atom(), number(), number()) :: number()
 
-  def area(shape, a, b//1) when a >= 0 and b >= 0 do
-    case shape do
-      :triangle -> a * b / 2
-      :rectangle -> a * b
-      :ellipse -> :math.pi() * a * b
-      _ -> 'you done fucked up'
-    end
+  def area(:rectangle, a, b) when a >= 0 and b >= 0 do
+    a * b
+  end
+
+  def area(:triangle, a, b) when a >= 0 and b >= 0 do
+    a * b / 2
+  end
+
+  def area(:ellipse, a, b) when a >= 0 and b >= 0 do
+    :math.pi * a * b
   end
 
 end
