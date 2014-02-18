1d12c093fda42381fd92310cb3c3854723d22f54
Alexander <alxndr+github@gmail.com>
2014-02-17 18:02:07 -0800

    elixir: etude 3-4, tuple as parameter

--- etudes-for-elixir/geom.ex
+++ etudes-for-elixir/geom.ex
@@ -2,18 +2,23 @@ defmodule Geom do
 
   @spec area(atom(), number(), number()) :: number()
 
-  def area(:rectangle, a, b) when a >= 0 and b >= 0 do
+  def area(tuple) do
+    { atom, a, b } = tuple
+    area(atom, a, b)
+  end
+
+  defp area(:rectangle, a, b) when a >= 0 and b >= 0 do
     a * b
   end
 
-  def area(:triangle, a, b) when a >= 0 and b >= 0 do
+  defp area(:triangle, a, b) when a >= 0 and b >= 0 do
     a * b / 2
   end
 
-  def area(:ellipse, a, b) when a >= 0 and b >= 0 do
+  defp area(:ellipse, a, b) when a >= 0 and b >= 0 do
     :math.pi * a * b
   end
 
-  def area(_atom, _a, _b), do: 0
+  defp area(_atom, _a, _b), do: 0
 
 end
