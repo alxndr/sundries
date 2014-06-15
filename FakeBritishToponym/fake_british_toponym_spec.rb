9226282b00b600f018cd38147636d79c84b6ced0
Alexander <alxndr+github@gmail.com>
2014-06-14 19:28:14 -0700

    more toponym sanity checks

--- FakeBritishToponym/fake_british_toponym_spec.rb
+++ FakeBritishToponym/fake_british_toponym_spec.rb
@@ -8,12 +8,11 @@ describe FakeBritishToponym do
     expect(subject).to be_a String
   end
 
-
   describe 'options' do
 
     describe 'modifier' do
       describe 'when set to false' do
-        it 'should never include antefix or postfix' do
+        it 'should never be more than one word or include punctuation' do
           MANY.times do
             toponym = FakeBritishToponym.new(modifier: false)
             expect(toponym).to_not include ' '
@@ -24,8 +23,14 @@ describe FakeBritishToponym do
       end
 
       it 'should not contain obvious errors' do
+        DOUBLE_CAPITAL_REGEX = /{[A-Z]{2}/
+        DOUBLE_NONALPHA_REGEX = /[^[:alpha:]]{2}/
+
         MANY.times do
           toponym = FakeBritishToponym.new(modifier: true)
+          [DOUBLE_CAPITAL_REGEX, DOUBLE_NONALPHA_REGEX].each do |regex|
+            expect(toponym).not_to match regex
+          end
           [' -', '- ', " '", "' "].each do |bad_char_sequence|
             expect(toponym).not_to include bad_char_sequence
           end
