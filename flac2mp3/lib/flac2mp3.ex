be5cea4bb1095eb8ec67ff3cdfb817ea658fe8de
Alexander <alxndr+github@gmail.com>
2014-09-27 18:22:14 -0700

    processing one flac now...
    [#6]

--- flac2mp3/lib/flac2mp3.ex
+++ flac2mp3/lib/flac2mp3.ex
@@ -3,25 +3,27 @@ defmodule Flac2mp3 do
   # assumes you have flac and lame available via $PATH
 
   def convert_flac(flacfile) do
+    IO.inspect "CONVERTING!"
     IO.inspect flacfile
     wavfile = String.replace(flacfile, ~r/\.flac$/, ".wav")
     mp3file = String.replace(flacfile, ~r/\.flac$/, ".mp3")
-    # spawn a process and do this...
-    System.cmd "flac", ["-d", "-o", wavfile, flacfile]
-    # ...when that is done spawn a process and do this...
-    System.cmd "lame", ["--abr", "320", wavfile, mp3file]
-    # ...when that is done spawn a process and do this...
-    System.cmd "rm", [wavfile]
+    convert_to_wav = Task.async(fn -> System.cmd("flac", ["-d", "-o", wavfile, flacfile]) end)
+    Task.await(convert_to_wav)
+    convert_to_mp3 = Task.async(fn -> System.cmd "lame", ["--abr", "320", wavfile, mp3file] end)
+    Task.await(convert_to_mp3)
+    clean_up = Task.async(fn -> System.cmd "rm", [wavfile] end)
+    Task.await(clean_up)
     # ...now we're done
   end
 
   def convert_dir(dir\\".") do
     dir |>
     Path.expand |>
-    Path.join "**/*.flac" |>
+    Path.join("**/*.flac") |>
     Path.wildcard |>
     IO.inspect |>
-    Enum.map &convert_flac/1
+    Enum.map(&convert_flac/1) |>
+    IO.inspect
   end
 
 end
